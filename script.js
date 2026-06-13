7a8da403b7be8f7150ddf2f1f0eb6bf0b32264f8
pavani2007 <pavanichandrapatla@gmail.com>
2026-06-13 17:15:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ window.onload = function () {
 
     initStorage();
 
-    await syncGoogleFormStudents();
+    syncGoogleFormStudents();
 
     const isLoggedIn =
         sessionStorage.getItem("isLoggedIn");
