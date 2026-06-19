60ea823d576726cee03daeddd49c91928ba7b4f9
pavani2007 <pavanichandrapatla@gmail.com>
2026-06-19 20:15:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1501,7 +1501,17 @@ y =
             pdf.lastAutoTable.finalY + 15;
     }
 
-    pdf.save(fileName);
+   const blob = pdf.output("blob");
+const url = URL.createObjectURL(blob);
+
+const a = document.createElement("a");
+a.href = url;
+a.download = fileName;
+document.body.appendChild(a);
+a.click();
+
+document.body.removeChild(a);
+URL.revokeObjectURL(url);
 }
 
 
